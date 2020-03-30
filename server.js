34e06213cc2cfe3cd865b6d44eaf938bee95653b
Mario alberto sanchez <mario_alberto_sanchez@hotmail.com>
2020-03-30 12:01:55 -0600

    modificaciones en server.js

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('EducacionApp' + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 
 //PathLocationStragety
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(_dirname + '/dist/index.html'));
+    res.sendFile(__dirname + '/src/index.html');
 });
 
 console.log('listeling');
+
+//Escuchar Peticiones
+app.listen(process.env.PORT || 8080, () => {
+    console.log(__dirname);
+    console.log('Express server puerto 8080: \x1b[32m%s\x1b[0m', 'online');
+});
