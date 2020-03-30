4333bb6a941a19737d45f446b7de0d01d3e9db7a
Mario alberto sanchez <mario_alberto_sanchez@hotmail.com>
2020-03-30 11:45:03 -0600

    listen server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(_dirname + '/dist'));
+app.use(express.static('EducacionApp' + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
